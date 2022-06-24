7671f23f2fd56ae7c77b706c51bb95d112506595
Priyanshu Rai <Priyanshu.Rai@ibm.com>
2022-06-24 23:24:46 +0530

    Added Roboto Font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Blog from "./Components/Blog";
 import Header from "./Components/Header";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
